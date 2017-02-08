239e30d8298a4d86f776193063a94b7a75421745
wuhoho <wumingho@gmail.com>
2017-02-08 16:39:44 +0800

    mission2 finish

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'uploads')));
-
 app.use('/', index);
 app.use('/users', users);
 app.use('/api',api);
