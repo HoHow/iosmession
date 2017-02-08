69ff4a506ca6e9e060805b7b12170abe5da9eec1
wuhoho <wumingho@gmail.com>
2017-02-08 16:34:32 +0800

    upload mission2

--- controller/mission2.js
+++ controller/mission2.js
@@ -1,22 +1,27 @@
+var fs                = require('fs');
+// var path              = require('path');
+var url               = require('url');
 exports.searchPokemon = function(req,res){
   var headers = req.headers;
   var name     = headers['name'];
 
   switch(name){
     case 'Bulbasaur':
-      return res.json({StatusCode:200,id:1,chinese:'妙蛙種子',english:'Bulbasaur',weight:6.9,err:''});
+      var aaa = req.hostname+'/001.jpg';
+      
+      return res.json({StatusCode:200,id:001,chinese:'妙蛙種子',english:'Bulbasaur',weight:6.9,url:aaa,err:''});
       break;
     case 'Ivysaur':
-      return res.json({StatusCode:200,id:2,chinese:'妙蛙草',english:'Ivysaur',weight:13.1,err:''});
+      return res.json({StatusCode:200,id:002,chinese:'妙蛙草',english:'Ivysaur',weight:13.1,url:'',err:''});
       break;
     case 'Venusaur':
-      return res.json({StatusCode:200,id:3,chinese:'妙蛙花',english:'Venusaur',weight:100.1,err:''});
+      return res.json({StatusCode:200,id:003,chinese:'妙蛙花',english:'Venusaur',weight:100.1,err:''});
       break;
     case 'Charmander':
-      return res.json({StatusCode:200,id:4,chinese:'小火龍',english:'Charmander',weight:8.5,err:''});
+      return res.json({StatusCode:200,id:004,chinese:'小火龍',english:'Charmander',weight:8.5,err:''});
       break;
     case 'Charmeleon':
-      return res.json({StatusCode:200,id:5,chinese:'火恐龍',english:'Charmeleon',weight:19,err:''});
+      return res.json({StatusCode:200,id:005,chinese:'火恐龍',english:'Charmeleon',weight:19,err:''});
       break;
     default:
       return res.json({StatusCode:400,err:'找不到該神奇寶貝'});
--- routes/mission2.js
+++ routes/mission2.js
@@ -1,6 +1,10 @@
 var express         = require('express');
-var router          = express.Router();
+
+var url             = require('url');
 var controller      = require('../controller/mission2');
+var router          = express.Router();
+
+
 
 router.get('/',function(req, res, next){
   
