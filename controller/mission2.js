4076ef2297ba0c1bc038588cc714bddb5c34c6c6
wuhoho <wumingho@gmail.com>
2017-02-09 11:27:13 +0800

    upload mission2

--- controller/mission2.js
+++ controller/mission2.js
@@ -3,23 +3,23 @@ exports.searchPokemon = function(req,res){
   var id     = headers['id'];
 
   switch(id){
-    case 'Bulbasaur':
+    case '001':
       var url = 'https://'+req.hostname+'/001.jpg';
       return res.json({StatusCode:200,id:001,chinese:'妙蛙種子',english:'Bulbasaur',weight:6.9,url:url});
       break;
-    case 'Ivysaur':
+    case '002':
       var url = 'https://'+req.hostname+'/002.jpg';
       return res.json({StatusCode:200,id:002,chinese:'妙蛙草',english:'Ivysaur',weight:13.1,url:url});
       break;
-    case 'Venusaur':
+    case '003':
       var url = 'https://'+req.hostname+'/003.jpg';
       return res.json({StatusCode:200,id:003,chinese:'妙蛙花',english:'Venusaur',weight:100.1,url:url});
       break;
-    case 'Charmander':
+    case '004':
       var url = 'https://'+req.hostname+'/004.jpg';
       return res.json({StatusCode:200,id:004,chinese:'小火龍',english:'Charmander',weight:8.5,url:url});
       break;
-    case 'Charmeleon':
+    case '005':
       var url = 'https://'+req.hostname+'/005.jpg';
       return res.json({StatusCode:200,id:005,chinese:'火恐龍',english:'Charmeleon',weight:19,url:url});
       break;
--- controller/mission3.js
+++ controller/mission3.js
@@ -1,31 +1,31 @@
 exports.searchPokemon = function(req,res){
   var id = req.body.id;
-  
- if(id !== null && id !== String){
+
+ 
     switch(id){
       case '001':
-        return res.json({StatusCode:200,id:1,chinese:'妙蛙種子',english:'Bulbasaur',weight:6.9,err:''});
+        return res.json({StatusCode:200,id:1,chinese:'妙蛙種子',english:'Bulbasaur',weight:6.9});
         break;
 
       case '002':
-        return res.json({StatusCode:200,id:2,chinese:'妙蛙草',english:'Ivysaur',weight:13.1,err:''});
+        return res.json({StatusCode:200,id:2,chinese:'妙蛙草',english:'Ivysaur',weight:13.1});
         break;
 
       case '003':
-        return res.json({StatusCode:200,id:3,chinese:'妙蛙花',english:'Venusaur',weight:100.1,err:''});
+        return res.json({StatusCode:200,id:3,chinese:'妙蛙花',english:'Venusaur',weight:100.1});
         break;
 
       case '004':
-        return res.json({StatusCode:200,id:4,chinese:'小火龍',english:'Charmander',weight:8.5,err:''});
+        return res.json({StatusCode:200,id:4,chinese:'小火龍',english:'Charmander',weight:8.5});
         break;
 
       case '005':
-        return res.json({StatusCode:200,id:5,chinese:'火恐龍',english:'Charmeleon',weight:19.0,err:''});
+        return res.json({StatusCode:200,id:5,chinese:'火恐龍',english:'Charmeleon',weight:19.0});
         break;
 
       default:
-        return res.json({StatusCode:400,err:'找不到ID'});
+        return res.json({StatusCode:400,err:'找不到該神奇寶貝'});
 
     }
-  }
+  
 }
