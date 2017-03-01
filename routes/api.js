dfc1f9ddd3223fb050f2410c82db34c3a7772bfa
wuhoho <wumingho@gmail.com>
2017-03-01 10:48:53 +0800

    debug m1

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,8 @@
 var express       = require('express');
 var mission1      = require('./mission1');
-var mission2      = require('./mission2');
-var mission3      = require('./mission3');
-var mission4      = require('./mission4');
+// var mission2      = require('./mission2');
+// var mission3      = require('./mission3');
+// var mission4      = require('./mission4');
 // var mission5      = require('./mission5');
 // var mission6      = require('./mission6');
 var router        = express.Router();
@@ -15,9 +15,9 @@ router.get('/',function(req, res, next){
 
 
 router.use('/mission1',mission1);
-router.use('/mission2',mission2);
-router.use('/mission3',mission3);
-router.use('/mission4',mission4);
+// router.use('/mission2',mission2);
+// router.use('/mission3',mission3);
+// router.use('/mission4',mission4);
 // router.use('/mission5',mission5);
 // router.use('/mission6',mission6);
 module.exports    = router;
