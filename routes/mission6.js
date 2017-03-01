0aad1ebb746a2acebec1ef2cdfe6e44dca890d01
wuhoho <wumingho@gmail.com>
2017-03-01 10:44:59 +0800

    delete mission5,6

--- routes/mission6.js
+++ routes/mission6.js
@@ -3,8 +3,12 @@ var photo         = require('../controller/photo');
 var router        = express.Router();
 var url          = require('url');
 router.get('/',function(req,res){
-
-  res.json({url:'https://'+req.hostname+'/mg3008.jpg'})
+  
+  
+  
+    res.json({url:'https://'+req.hostname+'/mg3008.jpg'})
+  
+  
   res.end();
 });
 
