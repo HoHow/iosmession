6660b9d387c76e01467847a8aefb556414d233ed
wuhoho <wumingho@gmail.com>
2017-03-02 18:09:25 +0800

    del mission

--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,6 @@ var mission1      = require('./mission1');
 var mission2      = require('./mission2');
 var mission3      = require('./mission3');
 var mission4      = require('./mission4');
-// var mission5      = require('./mission5');
-// var mission6      = require('./mission6');
 var router        = express.Router();
 
 
@@ -18,6 +16,5 @@ router.use('/mission1',mission1);
 router.use('/mission2',mission2);
 router.use('/mission3',mission3);
 router.use('/mission4',mission4);
-// router.use('/mission5',mission5);
-// router.use('/mission6',mission6);
+
 module.exports    = router;
