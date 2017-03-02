a91111d4b3658dd11c2ad0f8a30b8d7b256a62d0
wuhoho <wumingho@gmail.com>
2017-03-02 20:42:01 +0800

    新增mission6

--- controller/photo.js
+++ controller/photo.js
@@ -13,13 +13,13 @@ exports.getPhoto = function(req,res) {
     var form = new multiparty.Form({uploadDir:'./uploads'});
     form.parse(req, function(err,files) {      
       if(!files){
-          res.status(400).json({message:'請確認上傳圖片'});
+          res.status(400).json({status:"-2",message:'請確認上傳圖片'});
           return;
       }else{
         if(files === undefined || Object.keys(files).length === 0){
-          res.json({message:'圖片上傳失敗'});  
+          res.json({status:"-1",message:'圖片上傳失敗'});  
         }else{
-          res.json({message:'圖片上傳成功'});  
+          res.json({status:"1",message:'圖片上傳成功'});  
         }
       }
       
---
+++ routes/mission6.js
@@ -0,0 +1,16 @@
+var express       = require('express');
+var photo         = require('../controller/photo');
+var router        = express.Router();
+var url          = require('url');
+router.get('/',function(req,res){
+  
+  
+  
+    res.json({message:"下載成功",url:'https://'+req.hostname+'/mg3008.jpg'})
+  
+  
+  res.end();
+});
+
+
+module.exports = router;
